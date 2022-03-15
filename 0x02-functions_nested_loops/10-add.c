f1cc7a758977e1bbdadc586eb895be713d616243
smutindak <99264072+smutindak@users.noreply.github.com>
2022-03-16 02:37:04 +0300

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two integers and returns the result
+ * @a: an integer to add to b
+ * @b: an integer to add to a
+ * Return: Void.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
