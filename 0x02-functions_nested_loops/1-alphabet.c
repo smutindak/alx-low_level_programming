526dde88e4a6c45dca433516200ec1e48adfa653
smutindak <stephen.mutinda187@gmail.com>
2022-03-15 14:54:29 -0700

    formatted according to betty standards

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include "main.h"
+
 /**
- * main - This program is to print letters of the alphabet in lowercase
+ * print_alphabet - lowercase alphabet
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
 		_putchar (letter);
 		letter++;
 	}
-_putchar ('\n');    
-} 
+	_putchar ('\n');
+	return (0);
+}
