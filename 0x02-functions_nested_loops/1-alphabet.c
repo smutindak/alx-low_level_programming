b6161b3b98c890e77650e9196a8fa510807109bb
smutindak <99264072+smutindak@users.noreply.github.com>
2022-03-16 00:21:12 +0300

    Program to print letters in lowercase a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - This program is to print letters of the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar (letter);
+		letter++;
+
+	}
+	_putchar ('\n');
+    
+} 
