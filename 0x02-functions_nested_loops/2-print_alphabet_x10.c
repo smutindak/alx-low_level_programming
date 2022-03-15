845e82afde6dd08531296eaa3217e05d2bd456e6
smutindak <99264072+smutindak@users.noreply.github.com>
2022-03-16 01:16:58 +0300

    print alphabet in lower case 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_alphabet - lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void rint_alphabet_x10(void)
+{
+    char letter;
+    char b=0;
+
+    for(b=0; b<10;b++)
+    {
+        for(letter='a'; letter<='z';letter++)
+        putchar(letter);
+        putchar('\n');
+    }   
+    return (0);
+}
