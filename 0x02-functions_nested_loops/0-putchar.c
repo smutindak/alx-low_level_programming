520878cf476a9b5feae5b94916ac91dac577f655
smutindak <stephen.mutinda187@gmail.com>
2022-03-15 13:49:20 -0700

    added main.h and _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-     char letter[3] = 'ags';
-
-    putchar(letter);
+	_putchar('_')
 }
