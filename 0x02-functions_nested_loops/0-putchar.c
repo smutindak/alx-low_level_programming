00d377f3d79527ed1e0f3003db24c03269bde9c0
smutindak <99264072+smutindak@users.noreply.github.com>
2022-03-15 23:29:42 +0300

    Project for nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+     char letter[3] = 'ags';
+
+    putchar(letter);
+}
