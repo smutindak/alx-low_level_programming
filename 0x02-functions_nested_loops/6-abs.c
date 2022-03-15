13bd99b9cf7c9650fc6e4876fa17d114f0813c28
smutindak <99264072+smutindak@users.noreply.github.com>
2022-03-16 02:05:31 +0300

    print an absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ *_abs - Check main
+ * @x: An integer input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number x
+ */
+int _abs(int x)
+{
+	return abs(x);
+}
