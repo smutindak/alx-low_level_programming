fb98a4f2f78c669dbc2ea6a71506938ef57607a9
smutindak <stephen.mutinda187@gmail.com>
2022-03-15 15:42:08 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,14 +11,13 @@
  */
 int _islower(int c)
 {
-    char i;
-    int lower = 0;
+	char i;
+	int lower = 0;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == c)
 			lower = 1;
 	}
-
 	return (lower);
 }
