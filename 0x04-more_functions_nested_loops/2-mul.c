0191c56d5ede30fdabdd39e924086b87e100d45f
smutindak <99264072+smutindak@users.noreply.github.com>
2022-03-17 22:40:58 +0300

    Multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * mul - Multiply 2 integers
+ * @c: Multiply 2 integers
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+    int num1;
+    int num2;
+    int result;
+
+    result = num1 * num2;
+
+    return (result);
+    
+}
