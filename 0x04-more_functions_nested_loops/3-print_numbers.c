f897cd9bf4e25550e4a76496564c9f3c0703ddce
smutindak <stephen.mutinda187@gmail.com>
2022-03-17 13:08:48 -0700

    print 0 - 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,17 +5,16 @@
 
 /**
  * print_numbers - Print number 0-9 followed by a new line
- * @a : integer a
  * Return: Always 0.
  */
 
 void print_numbers(void)
 {
-    int a;
+	int a;
 
-    for (a = 0; a <= 9; a++)
-    {
-        _putchar('0' + a);
-    }
-    _purchar('\n');
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0' + a);
+	}
+	_purchar('\n');
 }
