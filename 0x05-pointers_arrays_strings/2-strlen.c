0af593bbd651d230b35fde033a011a242c56a9d4
smutindak <99264072+smutindak@users.noreply.github.com>
2022-03-21 22:45:29 +0300

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: - input the string to measure the length
+ * @length: - variable to output the length
+ */
+
+int _strlen(char *s)
+{
+	char *s;
+    int length;
+
+    length = _strlen(*s);
+
+    return (length);
+}
